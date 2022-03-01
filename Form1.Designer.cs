48ef9971f9337d3c19becc936b5cddc8a7e583b1
GypoTalamus <vidzhet@inbox.ru>
2022-03-01 21:58:59 +0700

    Version 0.22.03.01.2158

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,12 +36,25 @@ namespace MasterMind
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(67, 413);
+            this.button2.Location = new System.Drawing.Point(13, 425);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 64);
@@ -57,9 +70,9 @@ namespace MasterMind
             this.label6.Location = new System.Drawing.Point(13, 9);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(372, 26);
+            this.label6.Size = new System.Drawing.Size(378, 26);
             this.label6.TabIndex = 4;
-            this.label6.Text = "Компьютер загадал 4-значное число с ";
+            this.label6.Text = "Компьютер загадал    -значное число с ";
             // 
             // label8
             // 
@@ -75,7 +88,7 @@ namespace MasterMind
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(15, 78);
+            this.label2.Location = new System.Drawing.Point(12, 116);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(147, 26);
             this.label2.TabIndex = 7;
@@ -84,7 +97,7 @@ namespace MasterMind
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(250, 78);
+            this.textBox1.Location = new System.Drawing.Point(315, 108);
             this.textBox1.MaxLength = 4;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 34);
@@ -93,7 +106,7 @@ namespace MasterMind
             // richTextBox1
             // 
             this.richTextBox1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.richTextBox1.Location = new System.Drawing.Point(20, 133);
+            this.richTextBox1.Location = new System.Drawing.Point(13, 155);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
             this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
@@ -104,7 +117,7 @@ namespace MasterMind
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(264, 413);
+            this.button1.Location = new System.Drawing.Point(298, 425);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 64);
@@ -113,11 +126,139 @@ namespace MasterMind
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(210, 108);
+            this.maskedTextBox1.Mask = "0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 34);
+            this.maskedTextBox1.TabIndex = 15;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 26);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Очки:  ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(242, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 26);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "10";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(200, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 26);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "4";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox4);
+            this.panel1.Controls.Add(this.richTextBox3);
+            this.panel1.Controls.Add(this.richTextBox2);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(454, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(372, 345);
+            this.panel1.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(74, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(225, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Выберите уровень сложности";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(20, 54);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 36);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Лёгкий";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(20, 150);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 38);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Обычный";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(20, 249);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 37);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Сложный";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Enabled = false;
+            this.richTextBox2.Location = new System.Drawing.Point(123, 54);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(231, 84);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "Компьютер загадывает число из 4 неповторяющихся цифр, отсчёт до нуля идёт с 10 оч" +
+    "ков.";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Enabled = false;
+            this.richTextBox3.Location = new System.Drawing.Point(123, 150);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(231, 84);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "Компьютер загадывает число из 6 неповторяющихся цифр, отсчёт до нуля идёт с 20 оч" +
+    "ков.";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Enabled = false;
+            this.richTextBox4.Location = new System.Drawing.Point(123, 249);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(231, 84);
+            this.richTextBox4.TabIndex = 6;
+            this.richTextBox4.Text = "Компьютер загадывает число из 8 неповторяющихся цифр, отсчёт до нуля идёт с 40 оч" +
+    "ков.";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(417, 492);
+            this.ClientSize = new System.Drawing.Size(1061, 492);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
@@ -129,6 +270,8 @@ namespace MasterMind
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,6 +285,18 @@ namespace MasterMind
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,12 +12,11 @@ namespace MasterMind
 {
     public partial class Form1 : Form
     {
-        //int StartNumber = 1;
-        //int Otgadka = 0;
-
-        int[] StartNumber = new int[4];  // загадываемое число
-        int[] Otgadka = new int[4];  // Ваш ответ
-        int Counter = 1;
+        int[] StartNumber;  // загадываемое число
+        int[] Otgadka;  // Ваш ответ
+        int Counter = 1; //Счётчик ответов
+        int Score; //Счётчик очков
+        int Level; //Сложность игры
 
         int Byki = 0;
         int Korovy = 0;
@@ -26,50 +25,34 @@ namespace MasterMind
         public Form1()
         {
             InitializeComponent();
-            //int StartNumber = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            StartNumber = new int[4];
+            Otgadka = new int[4];
+            Score = 10;
         }
 
         private void button2_Click(object sender, EventArgs e) //кнопка Угадать
         {
-           //StartNumber = val * 1000 + val1 * 100 + val2 * 10 + val3;
-
-            //button2.Text = (val + " " + val1 + " " + val2 + " " + val3);
-
-            //Otgadka = Int32.Parse(textBox1.Text);
-
-            //button2.Text = " " + Otgadka + " ";
-
-            //richTextBox1.Text = "Ход " + StartNumber + ": Быки = " + Byki + ", Коровы = " + Korovy + "\r\n";
             richTextBox1.AppendText("Ход " + Counter + ": Быки = " + Byki + ", Коровы = " + Korovy + "\r\n");
             Counter += 1;
             richTextBox1.ScrollToCaret();
-
-            
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Создание объекта для генерации чисел
-            //Random rnd = new Random();
-
+         
             GenSetN(4, 10, StartNumber);
 
-            //val = rnd.Next(0, 10);  //Получить очередное случайное число
-            //val1 = rnd.Next(0, 10); //Получить очередное случайное число
-            //if (val1 == val) val1 = rnd.Next(0, 10);
-            //val2 = rnd.Next(0, 10); //Получить очередное случайное число
-            //if ((val2 == val) || (val2 == val1)) val2 = rnd.Next(0, 10);
-            //val3 = rnd.Next(0, 10); //Получить очередное случайное число
-            //if ((val3 == val) || (val3 == val1) || (val3 == val2)) val3 = rnd.Next(0, 10);
-
             richTextBox1.AppendText("Ход " + Counter + ": Быки = " + Byki + ", Коровы = " + Korovy + "\r\n");
-            //StartNumber = StartNumber + 1;
-            //label8.Text = val + " " + val1 + " " + val2 + " " + val3;
             //button1.Visible = false;
             //button2.Visible = true;
             Counter += 1;
-            label8.Text = StartNumber[0] + " " + StartNumber[1] + " " + StartNumber[2] + " " + StartNumber[3];
+            //label8.Text = StartNumber[0] + " " + StartNumber[1] + " " + StartNumber[2] + " " + StartNumber[3];
             richTextBox1.ScrollToCaret();
 
         }
